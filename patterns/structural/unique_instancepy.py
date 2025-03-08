931e2e2e7b114344cec607fcf4e1a233d8d52227
sharan122 <sharanraj037840@gmail.com>
2025-03-08 11:52:24 +0530

    Refined Singleton pattern for consistent instance creation.

---
+++ patterns/structural/unique_instancepy.py
@@ -0,0 +1,5 @@
+class Singleton:  _instance = None  def __new__(cls, *args, **kwargs):    if not cls._instance:      cls._instance = super(Singleton, cls).__new__(cls)    return cls._instance  def some_business_logic(self):    return 'Performing business logic'  
+
+singleton1 = Singleton()  singleton2 = Singleton()  
+
+assert singleton1 is singleton2
