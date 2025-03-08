cddc19eb4f531047cee70ffc61b5e0b562c14549
sharan122 <sharanraj037840@gmail.com>
2025-03-08 11:42:39 +0530

    Refactored shape classes to calculate areas dynamically.

---
+++ patterns/structural/shape_calculation.py
@@ -0,0 +1,21 @@
+class Shape:
+    def area(self):
+        raise NotImplementedError()
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+shapes = [Circle(5), Square(4)]
+areas = [shape.area() for shape in shapes]
+print(areas)
